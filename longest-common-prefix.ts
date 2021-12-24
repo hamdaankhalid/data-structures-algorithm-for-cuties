b04a63143ca02628749bb157c501a988ca302681
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2021-12-24 16:41:25 -0500

    longest-common-prefix

---
+++ longest-common-prefix.ts
@@ -0,0 +1,26 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+*/
+
+function longestCommonPrefix(strs: string[]): string {
+    let common = "";
+    let smallestWordLen: number = Math.min(...strs.map((word) => word.length));
+    
+    let counter = 0;
+    while(counter < smallestWordLen){
+        let match = strs[0][counter];
+        
+        for(let w of strs){
+            if(w[counter]!==match){
+                return common;
+            }
+        }
+        
+        common += match;
+        counter++
+    }
+    
+    return common;
+};
