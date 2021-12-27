8385b7d5c20313b3208a2fda8475b4061acb0e65
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2021-12-27 16:27:31 -0500

    symmetric

--- symmetric.ts
+++ symmetric.ts
@@ -13,7 +13,15 @@
  * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
  */
-
+class TreeNode {
+         val: number
+         left: TreeNode | null
+         right: TreeNode | null
+         constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {    *         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+            this.right = (right===undefined ? null : right)
+        }
+    }
 function isSymmetric(root: TreeNode | null): boolean {
     return check(root.left, root.right);
 };
