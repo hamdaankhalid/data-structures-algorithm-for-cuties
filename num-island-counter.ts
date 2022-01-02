425ebf61c0c9b0883b0b1b943951efda35ca2ac6
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-01-02 09:36:17 -0500

    num of islands completed

--- num-island-counter.ts
+++ num-island-counter.ts
@@ -1,13 +1,30 @@
+/*
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+*/
+
 function numIslands(grid: string[][]): number {
-    const marked = [];
     let islandCounter=0;
 
     for(let row = 0 ; row<grid.length ; row++){
         for(let col = 0 ; col<grid[0].length ; col++){
-            console.log(marked, row,col)
-            if( (grid[row][col]==="1") && (!marked.includes(`${row},${col}`)) ){
-                dfs(grid,row, col, marked);
-                islandCounter++;
+            
+            if( (grid[row][col]==="1")){
+               // console.log(marked)
+               grid =  dfs(grid, row, col); 
+               islandCounter++;
+                // console.log("*********")
             }
         }
     }
@@ -15,33 +32,40 @@ function numIslands(grid: string[][]): number {
     return islandCounter;
 };
 
-function dfs(grid: string[][], row:number, col:number, marked: any[]){
+function dfs(grid: string[][], row:number, col:number){
     const stack = [[row, col]];
+    
     while(stack.length>0){
+        // console.log(stack)
         let currNode = stack.pop();
-        
-        marked.push(`${row},${col}`);
+        let [row, col] = currNode;
+        grid[row][col] = '#'
         
         // get neighbors, if neighbors are 1's append them to stack
         let neighbors = getNeighbors(row, col, grid);
         
         for(let neighbor of neighbors){
+            // console.log(neighbors, 'for', row, col)
             if(neighbor!=null){
-            if((grid[neighbor[0]][neighbor[1]] === "1") && (!marked.includes(`${neighbor[0]},${neighbor[1]}`)) ){
-                stack.push(neighbor);
-                marked.push(`${neighbor[0]},${neighbor[1]}`)
+                
+                if(grid[neighbor[0]][neighbor[1]] === "1"){
+                    grid[neighbor[0]][neighbor[1]] = '#';
+                    stack.push(neighbor);
                 }
+                
             }
         }
     }
+    return grid;
 }
 
 
 function getNeighbors(row: number, col: number, grid: string[][]): any[]{
     const up = row > 0 ? [row-1,col] : null;
     const down = row < (grid.length-1) ? [row+1, col] : null;
+    
     const left = col > 0 ? [row, col-1] : null;
-    const right = col < (grid.length[0]-1) ?[row, col+1] : null;
+    const right = col < (grid[0].length-1) ?[row, col+1] : null;
     
     return [up, down, left, right];
 }
