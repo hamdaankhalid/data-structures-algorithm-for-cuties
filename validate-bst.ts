67b406723c73cf9f689c4b639ee15aa1382b73c3
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-02-18 20:27:28 -0800

    refresh bst validate

--- validate-bst.ts
+++ validate-bst.ts
@@ -1,17 +1,38 @@
-function isValidBST(root: TreeNode | null, low: number = -Infinity, high: number = Infinity): boolean {
-    if(root == null){
-        return true
-    }
-    
-    if( (root.val <= low) || (root.val >= high)){
-        return false
-    }
-    
-    // every node on left must be lower than the node
-    const isLeftValid = isValidBST(root.left, low, root.val);
-    // every node on right must be higher than the node
-    const isRightValid = isValidBST(root.right, root.val, high);
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
 
-    return isLeftValid && isRightValid;
+/*
+Valid if null
+otherwise check if contraints are fucked then false
+else
+check children
+*/
+function isValidBST(root: TreeNode | null, lowerLimit: number = -Infinity, upperLimit: number = Infinity): boolean {
+    // console.log(root, lowerLimit, upperLimit)
+    if(!root) {
+        return true;
+    }
+    
+    if((root.val <= lowerLimit) || (root.val >= upperLimit)){
+        return false;   
+    }
+    
+    // every root on left should be lesser than parent
+    const leftValid = isValidBST(root.left, lowerLimit, root.val);
+    
+    // every root on right should be more than parent
+    const rightValid = isValidBST(root.right, root.val, upperLimit);
+    
+    return leftValid && rightValid;
 };
-
