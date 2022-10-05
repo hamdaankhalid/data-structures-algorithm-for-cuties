54490328dc2cfbde4158d95246970fdef4b6de35
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-10-04 20:06:10 -0700

    Unstoppable

--- permutations.cpp
+++ permutations.cpp
@@ -1,28 +1,44 @@
 #include <vector>
 #include <math.h>
+#include <set>
+
 
 class Solution {
-	std::vector<int> buildPermutation(
-			std::vector<int>& nums,
-			std::vector<int>& running,
+	void buildPermutation(
+			std::vector<std::vector<int> >& result,
+			std::vector<int>& unselectedPool
+			)
+	{
+		if (unselectedPool.size() == 0) {
+            std::vector<int> nonce;
+            result.push_back(nonce);
+			return;
+		}
+		
+		for (int i = 0; i < unselectedPool.size(); i++) {
+			int pick = unselectedPool.at(i);
+			std::vector<int> removedCopy = unselectedPool;
+			removedCopy.erase(removedCopy.begin() + i);
 			
-			) {
-				
+			std::vector<std::vector<int> > innerResult;
+			buildPermutation(innerResult, removedCopy);
+
+			for (std::vector<int> inner : innerResult) {
+				std::vector<int> concatenatedRes = { pick };
+				concatenatedRes.insert(concatenatedRes.end(), inner.begin(), inner.end());
+				result.push_back(concatenatedRes);
+			}
+		}
 	}
+
 public:
     std::vector<std::vector<int> > permute(std::vector<int>& nums) {
-		std::vector<std::vector<int> > result;
-		std::vector<int> running;
-
-		for (int i = 0; i < nums.size(); i++) {
-			std::set<int> used;
-			used.add(
-			result.push_back(buildPermutation(nums, running));
-		}
-
-		return result;
-    }
+		std::vector<std::vector<int> > res;
+		buildPermutation(res, nums);
+		return res;
+	}
 };
+
 /**
  *	1 2 3 -> return a vector of all unique permutations
  *
@@ -39,6 +55,7 @@ public:
  *	
  *	build each sequence and then pass it into our results
  *	at completion return
+ *	
  *
  *	
  * /
