e23e4e1480c8ab845fbd224847c67fb1813cdeb9
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-24 15:29:20 -0800

    DP

--- climb-stairs.cpp
+++ climb-stairs.cpp
@@ -26,3 +26,27 @@ private:
         return res;
     }
 };
+
+
+class Solution2 {
+public:
+    int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int dp[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n+1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
