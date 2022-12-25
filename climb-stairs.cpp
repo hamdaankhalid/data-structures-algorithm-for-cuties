7ee79aa9e48f3d639ec70e055a904243f74b382b
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-24 16:31:41 -0800

    min cost q

--- climb-stairs.cpp
+++ climb-stairs.cpp
@@ -40,10 +40,9 @@ public:
         }
 
         int dp[n+1];
-        dp[0] = 0;
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i < n+1; i++) {
+        for (int i = 2; i < n+1; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
 
@@ -66,7 +65,6 @@ public:
         int q = 1;
 
         for (int i = 2; i < n+1; i++) {
-            std::cout << p << ", " << q << std::endl;
             int temp = p;
             int temptwo = q;
             p = q;
