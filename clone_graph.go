9d293e5904688acf98a27404f1b0924c5291eedf
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-01-11 11:17:08 -0800

    rev ll" git push

--- clone_graph.go
+++ clone_graph.go
@@ -1,1 +1,44 @@
 package dsa_but_make_it_classy
+
+type Node struct {
+	Val       int
+	Neighbors []*Node
+}
+
+func cloneGraph(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+
+	mapping := make(map[int]*Node)
+
+	clone(node, mapping)
+
+	return mapping[node.Val]
+}
+
+// DFS through graph create a clone of the current node or pick it up from mapping if it already exists
+// iterate through nodes neighbors and if we havent already made a clone that neighbor clone it otherwise
+// attach the precloned object to it
+func clone(node *Node, mapping map[int]*Node) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if _, exists := mapping[node.Val]; !exists {
+		mapping[node.Val] = &Node{Val: node.Val}
+	}
+
+	clonedNode := mapping[node.Val]
+
+	for _, option := range node.Neighbors {
+		if premapped, exists := mapping[option.Val]; exists {
+			clonedNode.Neighbors = append(clonedNode.Neighbors, premapped)
+		} else {
+			clonedNeighbor := clone(option, mapping)
+			clonedNode.Neighbors = append(clonedNode.Neighbors, clonedNeighbor)
+		}
+	}
+
+	return clonedNode
+}
---
+++ reverse_ll.go
@@ -0,0 +1,86 @@
+package dsa_but_make_it_classy
+
+import (
+	"bufio"
+	"fmt"
+)
+
+package main
+
+import (
+"bufio"
+"fmt"
+"io"
+"os"
+"strconv"
+"strings"
+)
+
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+	head *SinglyLinkedListNode
+	tail *SinglyLinkedListNode
+}
+
+func (singlyLinkedList *SinglyLinkedList) insertNodeIntoSinglyLinkedList(nodeData int32) {
+	node := &SinglyLinkedListNode {
+		next: nil,
+		data: nodeData,
+	}
+
+	if singlyLinkedList.head == nil {
+		singlyLinkedList.head = node
+	} else {
+		singlyLinkedList.tail.next = node
+	}
+
+	singlyLinkedList.tail = node
+}
+
+func printSinglyLinkedList(node *SinglyLinkedListNode, sep string, writer *bufio.Writer) {
+	for node != nil {
+		fmt.Fprintf(writer, "%d", node.data)
+
+		node = node.next
+
+		if node != nil {
+			fmt.Fprintf(writer, sep)
+		}
+	}
+}
+
+/*
+ * Complete the 'reverse' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data int32
+ *     next *SinglyLinkedListNode
+ * }
+ *
+ */
+
+func reverse(llist *SinglyLinkedListNode) *SinglyLinkedListNode {
+	// Write your code here
+	var prev *SinglyLinkedListNode = nil
+	curr := llist
+	for curr != nil {
+		nextUp := curr.next
+		curr.next = prev
+		prev = curr
+		curr = nextUp
+	}
+	return prev
+}
+
+func main() {
