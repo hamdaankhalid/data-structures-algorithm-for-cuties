1880a83559b7defc91e7453fca94ac9d967741df
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-01-11 17:08:48 -0800

    way back from work

---
+++ insert_node_at_pos.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+)
+
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+	head *SinglyLinkedListNode
+	tail *SinglyLinkedListNode
+}
+
+func (singlyLinkedList *SinglyLinkedList) insertNodeIntoSinglyLinkedList(nodeData int32) {
+	node := &SinglyLinkedListNode{
+		next: nil,
+		data: nodeData,
+	}
+
+	if singlyLinkedList.head == nil {
+		singlyLinkedList.head = node
+	} else {
+		singlyLinkedList.tail.next = node
+	}
+
+	singlyLinkedList.tail = node
+}
+
+func printSinglyLinkedList(node *SinglyLinkedListNode, sep string, writer *bufio.Writer) {
+	for node != nil {
+		fmt.Fprintf(writer, "%d", node.data)
+
+		node = node.next
+
+		if node != nil {
+			fmt.Fprintf(writer, sep)
+		}
+	}
+}
+
+/*
+ * Complete the 'insertNodeAtPosition' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER data
+ *  3. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data int32
+ *     next *SinglyLinkedListNode
+ * }
+ *
+ */
+
+func insertNodeAtPosition(llist *SinglyLinkedListNode, data int32, position int32) *SinglyLinkedListNode {
+	// Write your code here
+	var prev *SinglyLinkedListNode = nil
+	curr := llist
+	var idx int32
+	for idx = 0; idx < position; idx++ {
+		prev = curr
+		curr = curr.next
+	}
+	prev.next = &SinglyLinkedListNode{next: curr, data: data}
+	return llist
+}
